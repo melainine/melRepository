02f1f70975720d321980c5eee831cdd8563d63fa
mel <mel3ainine@ymail.com>
2020-06-17 17:01:34 +0200

    everything works

--- src/main/java/microservice/config/SecurityConfig.java
+++ src/main/java/microservice/config/SecurityConfig.java
@@ -3,15 +3,23 @@ package microservice.config;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.Filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +27,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
@@ -27,9 +36,12 @@ import org.springframework.security.oauth2.client.token.grant.code.Authorization
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.CompositeFilter;
 
 @EnableOAuth2Client
 @Configuration
+@PropertySource("classpath:social.properties")
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Value("${client-id}")
@@ -41,9 +53,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Value("${access-token-uri}")
 	private String accessTokenUri;
 	
+	@Autowired
+	OAuth2ClientContext oauth2ClientContext;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class).authorizeRequests();
 		http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+		http
+        .formLogin()
+        .loginPage("/loginpage")
+      .and()
+        .logout()
+        .logoutSuccessUrl("/");
 	}
 	
 	@Bean
@@ -136,5 +158,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		registration.setFilter(filter);
 		registration.setOrder(-100);
 		return registration;
+	}
+	private Filter ssoFilter() {
+
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+
+
+		OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				"/oauth2/google");
+		OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google(), oauth2ClientContext);
+		googleFilter.setRestTemplate(googleTemplate);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(googleResource().getUserInfoUri(), google().getClientId());
+		tokenServices.setRestTemplate(googleTemplate);
+		googleFilter.setTokenServices(tokenServices);
+
+		filters.add(googleFilter);
+
+		filter.setFilters(filters);
+
+		return filter;
+	}
+	@Bean
+	@ConfigurationProperties(prefix = "security.oauth2.client")
+	public AuthorizationCodeResourceDetails google() {
+		return new AuthorizationCodeResourceDetails();
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "security.oauth2.resource")
+	public ResourceServerProperties googleResource() {
+		return new ResourceServerProperties();
 	}
 }
--- src/main/java/microservice/controller/GoogleController.java
+++ src/main/java/microservice/controller/GoogleController.java
@@ -1,85 +1,131 @@
 package microservice.controller;
 
-
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.Filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.social.google.api.Google;
+import org.springframework.social.google.api.impl.GoogleTemplate;
 import org.springframework.social.google.api.plus.Person;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import microservice.model.AppUser;
-import microservice.service.SocialService;
+import microservice.model.Device;
+import microservice.repository.DeviceRepository;
+import microservice.service.UserService;
 
-@Controller
+@RestController
 public class GoogleController {
+
 	@Autowired
-	private SocialService googleService;
+	private UserService userService;
 
-	@GetMapping(value = "/oauth2/google")
-	public RedirectView googlelogin() {
-		RedirectView redirectView = new RedirectView();
-		String url = googleService.googlelogin();
-		redirectView.setUrl(url);
-		return redirectView;
+	@Autowired
+	private DeviceRepository deviceRepository;
 
-	}
-
-	@GetMapping(value = "/google")
-	public String google(@RequestParam("code") String code) {
-		String accessToken = googleService.getGoogleAccessToken(code);
-		return "redirect:/googleprofiledata/" + accessToken;
-
-	}
-
-	@GetMapping(value = "/googleprofiledata/{accessToken:.+}")
-	public ModelAndView googleprofiledata(@PathVariable String accessToken, ModelAndView mv,HttpServletRequest request, 
-		      HttpServletResponse response, Authentication authentication, Principal principal) {
-		HttpSession session = request.getSession(false);
-		System.out.println("____________Session:_______"+session);
-		System.out.println("____________principal:_______"+principal);
-
-		System.out.println("____________Authentication:_______"+authentication);
-		Person user = googleService.getGoogleUserProfile(accessToken);
-		System.out.println("____________User:_______"+user);
-
-
-		//AppUser user = new AppUser(authentication.getName(),authentication.getName(),authentication.getName());
-		mv.setViewName("home");
-		mv.addObject("loggedin", true);
-//		System.out.println("_______User_______:"+user);
-//		mv.addObject("name", user);
-
-		return mv ;
-
-//		try {
-//			Person user = googleService.getGoogleUserProfile(accessToken);
+//	@RequestMapping("/googleuser")
+//	public Principal user(Principal principal, ModelAndView mv) {
+//		mv.setViewName("home");
+//		mv.addObject("loggedin", true);
 //
-//			mv.setViewName("home");
-//			mv.addObject("loggedin", true);
-//			System.out.println("the user firstname is: " +user.getGivenName());
-//			mv.addObject("cover", user.getImageUrl());
-//			mv.addObject("name", user.getGivenName()+ " " + user.getFamilyName());
-//			return mv;
-//			
-//		} catch (Exception e) {
-//			mv.addObject("loggedinerror", true);
-//			mv.addObject("message", e.getMessage());
-//			return mv ;
+//		OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+//		Authentication authentication = oAuth2Authentication.getUserAuthentication();
+//		Map<String, String> details = new LinkedHashMap<>();
+//		details = (Map<String, String>) authentication.getDetails();
+//
+//		System.out.println("_________type__________"+principal.getClass()+" ,");
+//		AppUser dbUser = userService.findByEmail(details.get("email"));
+//		if (dbUser != null) {
+//
+//			mv.addObject("hasDevices", true);
+//			mv.addObject("devices", deviceRepository.findAllByEmail(dbUser.getEmail()));
+//
+//			return principal;
+//
+//		} else {
+//			String role = "GOOGLE_USER";
+//			AppUser appUser = new AppUser(details.get("given_name"), details.get("family_name"), details.get("email"));
+//			appUser.setRole(role);
+//
+//			userService.save(appUser);
+//
+//			List <Device >devices = deviceRepository.findAllByEmail(appUser.getEmail());
+//			mv.addObject("hasDevices", true);
+//			mv.addObject("devices", deviceRepository.findAllByEmail(appUser.getEmail()));
+//
+//			return principal;
 //		}
-		
-	}
+//
+//	}
+	@RequestMapping("/googleuser")
 
+	public Map<Object, Object> googleUser(Principal principal, ModelAndView mv) {
+		mv.setViewName("home");
+		//mv.addObject("loggedin", true);
+
+		OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+		Authentication authentication = oAuth2Authentication.getUserAuthentication();
+		Map<Object, Object> details = new LinkedHashMap<>();
+		details = (Map<Object, Object>) authentication.getDetails();
+		System.out.println("_____________Details___________" + details.get("email"));
+
+
+		AppUser dbUser = userService.findByEmail((String) details.get("email"));
+
+		if (dbUser != null) {
+
+			mv.addObject("hasDevices", true);
+			mv.addObject("devices", deviceRepository.findAllByEmail(dbUser.getEmail()));
+			details.put("devices", deviceRepository.findAllByEmail(dbUser.getEmail()));
+
+
+			return details;
+
+		}
+		String role = "GOOGLE_USER";
+		AppUser appUser = new AppUser((String) details.get("given_name"), (String) details.get("family_name"), (String) details.get("email"));
+		appUser.setRole(role);
+
+		userService.save(appUser);
+
+		List<Device> devices = deviceRepository.findAllByEmail(appUser.getEmail());
+		mv.addObject("hasDevices", true);
+		mv.addObject("devices", deviceRepository.findAllByEmail(appUser.getEmail()));
+
+		return details;
+
+	}
 
 }
